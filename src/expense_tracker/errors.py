79eb6be41d694562fbcb364de921b4f1ad353cbd
amit-rajpoot <amitrajpoot33810@gmail.com>
2026-09-26 01:57:48 +0530

    Complete custom error definitions

--- src/expense_tracker/errors.py
+++ src/expense_tracker/errors.py
@@ -1,13 +1,16 @@
 # <------------ BASE ERROR ------------>
+
 class ExpenseTrackerError(Exception):
     """Base exception for expense tracker."""
 
 
 # <------------ VALIDATION ERROR ------------>
+
 class ValidationError(ExpenseTrackerError):
     """Raised when user input is invalid."""
 
 
 # <------------ STORAGE ERROR ------------>
+
 class StorageError(ExpenseTrackerError):
     """Raised when storage operation fails."""
